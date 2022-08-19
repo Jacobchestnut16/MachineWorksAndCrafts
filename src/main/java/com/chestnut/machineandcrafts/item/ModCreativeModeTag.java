6d6832cd4f783215f240ba51ba97c7fa762ae985
jacobchestnut <e16.jmc@gmail.com>
2022-08-18 20:46:33 -0400

    Added Stone age, Metal, Electricity; items

--- src/main/java/com/chestnut/machineandcrafts/item/ModCreativeModeTag.java
+++ src/main/java/com/chestnut/machineandcrafts/item/ModCreativeModeTag.java
@@ -18,4 +18,25 @@ public class ModCreativeModeTag {
             return new ItemStack(ModBlocks.PULVERIZED_IRON_ORE.get());
         }
     };
+
+    public static final CreativeModeTab ELECTRICITY = new CreativeModeTab("electricity") {
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(ModItems.COPPER_WIRE.get());
+        }
+    };
+
+    public static final CreativeModeTab STONE_AGE = new CreativeModeTab("stone_age") {
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(ModItems.SIFTING_FRAME.get());
+        }
+    };
+
+    public static final CreativeModeTab METAL = new CreativeModeTab("metal") {
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(ModItems.COPPER.get());
+        }
+    };
 }
--- src/main/java/com/chestnut/machineandcrafts/item/ModItems.java
+++ src/main/java/com/chestnut/machineandcrafts/item/ModItems.java
@@ -10,6 +10,32 @@ import net.minecraftforge.registries.RegistryObject;
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MachineAndCrafts.MODID);
 
+
+    //Stone age
+    public static final RegistryObject<Item> CHICKEN_WIRE = ITEMS.register("chicken_wire",
+            ()-> new Item(new Item.Properties().tab(ModCreativeModeTag.STONE_AGE)));
+
+    public static final RegistryObject<Item> SIFTING_FRAME = ITEMS.register("sifting_frame",
+            ()-> new Item(new Item.Properties().tab(ModCreativeModeTag.STONE_AGE)));
+
+
+    //Metal
+    public static final RegistryObject<Item> COPPER = ITEMS.register("copper",
+            ()-> new Item(new Item.Properties().tab(ModCreativeModeTag.METAL)));
+
+    public static final RegistryObject<Item> NICKEL = ITEMS.register("nickel",
+            ()-> new Item(new Item.Properties().tab(ModCreativeModeTag.METAL)));
+
+
+    //Electricity
+    public static final RegistryObject<Item> COPPER_WIRE = ITEMS.register("copper_wire",
+            ()-> new Item(new Item.Properties().tab(ModCreativeModeTag.ELECTRICITY)));
+
+    public static final RegistryObject<Item> WIRE = ITEMS.register("wire",
+            ()-> new Item(new Item.Properties().tab(ModCreativeModeTag.ELECTRICITY)));
+
+
+    //Pulverized
     public static final RegistryObject<Item> PULVERIZED_IRON = ITEMS.register("pulverized_iron",
             ()-> new Item(new Item.Properties().tab(ModCreativeModeTag.PULVERIZED_DUST)));
 
